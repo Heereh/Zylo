5e15aeb476fc84d4425b090349649ecf3aba59ef
Heredik <nahuel.arevalo@hotmail.com.ar>
2025-11-06 13:38:01 -0300

    feat(ui):Rediseño del Button, Nuevos color Default, nueva props de Icon,IconPosition

--- src/component/iu/CustomButton/CustomButton.tsx
+++ src/component/iu/CustomButton/CustomButton.tsx
@@ -1,40 +1,56 @@
-import React from 'react';
-import './CustomButton.css';
+import React from "react";
+import "./CustomButton.css";
 
 interface CustomButtonProps {
-  size?: 'small' | 'mediun' | 'large';
+  size?: "small" | "mediun" | "large";
   backgroundColor?:
-    | 'primary'
-    | 'secondary'
-    | 'danger'
-    | 'neutral'
-    | 'success'
-    | 'custom';
-  appearance?: 'solid' | 'outline' | 'ghost';
+    | "default"
+    | "primary"
+    | "secondary"
+    | "danger"
+    | "neutral"
+    | "success"
+    | "custom";
+  appearance?: "solid" | "outline" | "ghost";
   children: React.ReactNode;
-  type?: 'button' | 'submit' | 'reset';
+  type?: "button" | "submit" | "reset";
   onClick?: () => void;
-  disabled?: boolean
+  disabled?: boolean;
+  icon?: React.ReactNode;
+  iconPosition?: "start" | "end";
 }
 
 const CustomButton: React.FC<CustomButtonProps> = ({
-	size = 'small',
-	backgroundColor = 'primary',
-	appearance = 'solid',
-	children,
-	type,
-	onClick,
-	disabled 
+  size = "small",
+  backgroundColor = "primary",
+  appearance = "solid",
+  children,
+  type = "button",
+  onClick,
+  disabled,
+  icon,
+  iconPosition = "start",
 }) => {
-	const className = `custom-button ${size} ${backgroundColor} ${appearance}`;
-	return (
+  const className = `custom-button ${size} ${backgroundColor} ${appearance}`;
+  const startContent =
+    icon && iconPosition === "start" ? (
+      <span className="custom-button__content">{icon}</span>
+    ) : null;
+
+  const endContent =
+    icon && iconPosition === "end" ? (
+      <span className="custom-button__content">{icon}</span>
+    ) : null;
+  return (
     <button
       className={className}
       type={type}
-      onClick={onClick} // Llamamos al onClick que se pasa como props
-      disabled ={disabled}
+      onClick={onClick} //
+      disabled={disabled}
     >
+      {startContent}
       {children}
+      {endContent}
     </button>
   );
 };
