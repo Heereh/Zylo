c96b3bd5aca0d8b057f55957421cba50b7adc289
Heredik <nahuel.arevalo@hotmail.com.ar>
2025-11-06 13:32:09 -0300

    fix(code):Se borro codigo innecesario

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,8 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-/* import { defaultSystem, ChakraProvider } from '@chakra-ui/react'; */
-/* import { ChakraProvider } from '@chakra-ui/react'; */
 import App from './App.tsx';
 import './styles/reset.css';
 import './styles/globals.css';
-/* import { BrowserRouter } from 'react-router'; */
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
