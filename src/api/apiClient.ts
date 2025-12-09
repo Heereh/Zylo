ce301559c28349a1f4bc232868271403ad7e234d
Heereh <nahuel.arevalo@hotmail.com.ar>
2025-12-08 21:09:55 -0300

    chore:actualizar versiones de dependencia

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -1,38 +1,38 @@
-import axios from "axios";
-import { useAuthStore } from "../store/GymUserStore";
+import axios from 'axios';
+import { useAuthStore } from '../store/GymUserStore';
 
 const authApi = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + `/api/v1`,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	baseURL: import.meta.env.VITE_API_URL + `/api/v1`,
+	headers: {
+		'Content-Type': 'application/json',
+	},
 });
 
 authApi.interceptors.request.use(
-  (config) => {
-    const token = useAuthStore.getState().accessToken;
+	(config) => {
+		const token = useAuthStore.getState().accessToken;
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => {
-    return Promise.reject(error);
-  },
+		if (token) {
+			config.headers.Authorization = `Bearer ${token}`;
+		}
+		return config;
+	},
+	(error) => {
+		return Promise.reject(error);
+	},
 );
 
 authApi.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  (error) => {
-    if (error.response && error.response.status === 401) {
-      localStorage.removeItem("accessToken");
-      window.location.href = "/login";
-    }
-    return Promise.reject(error);
-  },
+	(response) => {
+		return response;
+	},
+	(error) => {
+		if (error.response && error.response.status === 401) {
+			localStorage.removeItem('accessToken');
+			window.location.href = '/login';
+		}
+		return Promise.reject(error);
+	},
 );
 
 export default authApi;
